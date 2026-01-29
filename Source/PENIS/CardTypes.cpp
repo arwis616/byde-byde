4d136e5ca3748ec56bbcee479f272b59dd54ca18
arwis616 <denis67sennikov@gmail.com>
2026-01-29 10:16:09 +0300

    fixed struct mismatch issues update W_Hand logic

--- Source/PENIS/CardTypes.cpp
+++ Source/PENIS/CardTypes.cpp
@@ -1,6 +1,5 @@
 #include "CardTypes.h"
 
-// --- ФУНКЦИЯ 1: Создание колоды ---
 TArray<FCardData> UCardTypes::CreateStarterDeck(int32 Count, UTexture2D* DefaultIcon)
 {
 	TArray<FCardData> NewDeck;
@@ -9,18 +8,28 @@ TArray<FCardData> UCardTypes::CreateStarterDeck(int32 Count, UTexture2D* Default
 	{
 		FCardData NewCard;
 
-		FString NameString = FString::Printf(TEXT("C++ Card %d"), i);
+		// нейм и иконка
+		FString NameString = FString::Printf(TEXT("Card %d"), i);
 		NewCard.DisplayName = FText::FromString(NameString);
-		NewCard.ManaCost = FMath::RandRange(1, 10);
 		NewCard.Icon = DefaultIcon;
-		// Безопасное приведение типа для Enum
+
+		// рандомная цена маны(1-5)
+		NewCard.ManaCost = FMath::RandRange(1, 5);
+
+		// рандомное действие (0 - дмг, 1 - хилл, 2 - щит)
+		int32 RandomAction = FMath::RandRange(0, 2);
+		NewCard.ActionType = static_cast<ECardAction>(RandomAction);
+
+		// рандомная сила (5-15)
+		NewCard.EffectValue = FMath::RandRange(5, 15);
+
 		NewCard.Element = static_cast<ECardElement>((i % 4) + 1);
 
 		NewDeck.Add(NewCard);
 	}
 
 	return NewDeck;
-} 
+}
 
 bool UCardTypes::CanPlayCard(int32 CurrentMana, int32 CardCost)
 {
@@ -31,5 +40,29 @@ bool UCardTypes::CanPlayCard(int32 CurrentMana, int32 CardCost)
 	else
 	{
 		return false;
+	}
+}
+
+void UCardTypes::ProcessCardAction(ECardAction Action, int32 Power, int32 CurrentEnemyHP, int32 CurrentPlayerHP, int32& OutEnemyHP, int32& OutPlayerHP)
+{
+	OutEnemyHP = CurrentEnemyHP;
+	OutPlayerHP = CurrentPlayerHP;
+
+	// проверка типа карты
+	switch (Action)
+	{
+	case ECardAction::Damage:
+		OutEnemyHP = CurrentEnemyHP - Power; // отнимаем, если дмг
+		if (OutEnemyHP < 0) OutEnemyHP = 0;  // чтобы не уходило в минус
+		break;
+
+	case ECardAction::Heal:
+		OutPlayerHP = CurrentPlayerHP + Power; // прибавляем, если хилл
+		// можно добавить ограничение по макс хп позже
+		break;
+
+	case ECardAction::Shield:
+		// логика щита позже
+		break;
 	}
 }
--- Source/PENIS/CardTypes.h
+++ Source/PENIS/CardTypes.h
@@ -2,19 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "Engine/Texture2D.h"
-#include "CardTypes.generated.h" // ЭТОТ INCLUDE ВСЕГДА ДОЛЖЕН БЫТЬ ПОСЛЕДНИМ!
-
-// --- 1. ENUMS ---
+#include "CardTypes.generated.h"
 
 UENUM(BlueprintType)
-enum class ECardRarity : uint8
+enum class ECardAction : uint8
 {
-	Common UMETA(DisplayName = "Common"),
-	Rare UMETA(DisplayName = "Rare"),
-	Epic UMETA(DisplayName = "Epic"),
-	Legendary UMETA(DisplayName = "Legendary"),
-	Mythic UMETA(DisplayName = "Mythic")
+	Damage  UMETA(DisplayName = "Deal Damage"), // дмг
+	Heal    UMETA(DisplayName = "Restore Health"), // хил
+	Shield  UMETA(DisplayName = "Add Shield") // щит
 };
 
 UENUM(BlueprintType)
@@ -27,42 +22,31 @@ enum class ECardElement : uint8
 	Air UMETA(DisplayName = "Air")
 };
 
-// --- 2. STRUCT ---
-
 USTRUCT(BlueprintType)
 struct FCardData
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FText DisplayName;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
-	FText Description;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
-	int32 ManaCost;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UTexture2D* Icon;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
-	ECardRarity Rarity;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ManaCost;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Data")
+	// тип карты
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ECardAction ActionType;
+
+	// сила
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 EffectValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	ECardElement Element;
-
-	// Конструктор
-	FCardData()
-	{
-		ManaCost = 0;
-		Rarity = ECardRarity::Common;
-		Element = ECardElement::None;
-		Icon = nullptr;
-	}
 };
-
-// --- 3. CLASS ---
 
 UCLASS()
 class PENIS_API UCardTypes : public UBlueprintFunctionLibrary
@@ -70,11 +54,12 @@ class PENIS_API UCardTypes : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-
 	UFUNCTION(BlueprintCallable, Category = "Card Game C++")
 	static TArray<FCardData> CreateStarterDeck(int32 Count, UTexture2D* DefaultIcon);
 
-	// Проверка маны
-	UFUNCTION(BlueprintCallable, Category = "Card Game C++") 
+	UFUNCTION(BlueprintCallable, Category = "Card Game C++")
 	static bool CanPlayCard(int32 CurrentMana, int32 CardCost);
+
+	UFUNCTION(BlueprintCallable, Category = "Card Game C++")
+	static void ProcessCardAction(ECardAction Action, int32 Power, int32 CurrentEnemyHP, int32 CurrentPlayerHP, int32& OutEnemyHP, int32& OutPlayerHP);
 };
